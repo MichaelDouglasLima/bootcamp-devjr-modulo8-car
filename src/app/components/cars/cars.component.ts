bc27d1ecbe82f0ae99a7356ed8fa5eebd0aa8c2e
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2024-01-09 08:21:20 -0300

    closes #8 - Loaded Cars

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -10,9 +10,31 @@ export class CarsComponent {
 
   car : Car = { } as Car;
   isUpdate : boolean = false;
-  idCount : number = 1;
+  idCount : number = 4;
 
-  cars : Car[]  = [];
+  cars : Car[]  = [
+    {
+      id: 1,
+      name: " Veloz 2000",
+      automaker: "SuperCars Inc.",
+      price: 250000,
+      year: 2022
+    },
+    {
+      id: 2,
+      name: "Elegante 500",
+      automaker: "Luxo Motors",
+      price: 120000,
+      year: 2021
+    },
+    {
+      id: 3,
+      name: "Aventureiro X7",
+      automaker: "OffRoad Vehicles",
+      price: 180000,
+      year: 2023
+    }
+  ];
 
   saveCar(){
 
